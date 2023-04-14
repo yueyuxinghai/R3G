ae5664c661abdc430f70ffb84ab401240fc32710
yueyuxinghai <83649974+yueyuxinghai@users.noreply.github.com>
2023-04-14 10:20:49 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -27,9 +27,9 @@ pushd package/community
 #rm -rf ../lean/luci-app-kodexplorer
 
 # Add immortalwrt's Packages
-git clone --depth=1 -b openwrt-18.06 https://github.com/immortalwrt/packages
-git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/luci
-cp -r ../luci/applications/luci-app-adguardhome ../package/lean//luci-app-adguardhome
+#git clone --depth=1 -b openwrt-18.06 https://github.com/immortalwrt/packages
+#git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/immortalwrt/luci
+#cp -r ../luci/applications/luci-app-adguardhome ../package/lean//luci-app-adguardhome
 
 # Add luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
